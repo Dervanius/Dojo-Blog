fd7d1a57822be84574084ab11a30c6f401ecd4a2
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-16 11:57:32 +0100

    Redirect using history

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,6 +25,9 @@ const Create = () => {
     }).then(() => {
       console.log("New blog added");
       setIsPending(false);
+      //go to previous page
+      //history.go(-1);
+      history.push("/");
     });
   };
 
