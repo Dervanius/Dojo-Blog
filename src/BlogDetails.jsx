e8d0a4adb843b6b90f01466bbdcd6af9573f8ada
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-16 12:09:57 +0100

    Delete function added

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,14 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 import Spinner from "./Spinner";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -24,6 +33,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
