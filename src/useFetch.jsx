d070fa91e383c47c579b81c37b59b825818b5bc2
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-16 09:53:59 +0100

    Routing added

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -23,7 +23,7 @@ const useFetch = (url) => {
           setIsPending(false);
           setError(err.message);
         });
-    }, 2000);
+    }, 1000);
   }, []);
 
   return { data, isPending, error };
